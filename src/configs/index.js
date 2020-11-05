fb5445b4d3ed0bc49afbef33da7888fe262e591a
Antonio Pierro <antonio.pierro@gmail.com>
2020-11-05 16:54:53 +0100

    adding anding.template

--- src/configs/index.js
+++ src/configs/index.js
@@ -3,6 +3,19 @@ const data = {
     title2: "My title2",
     name: "My name is anton",
     loginRegister: "Connexion / Inscription",
+    enterYourEmailToContinue: "Renseignez votre adresse email pour continuer.",
+    yourEmail: "Votre email",
+    signIn: "Se Connecter",
+    signUp: "S'inscrire",
+    // fill-info-to-create-account: Renseignez vos informations pour créer votre compte Franklin.
+    // your-first-name: Votre nom
+    // your-last-name: Your last name
+    // your-password: Votre mot de passe
+    // confirm-your-password: Confirmez votre mot de passe
+    // create-account: Créer un compte
+    // subscribe-newsletter: Je m'inscris à la newsletter Franklin
+    login: "Connexion",
+    // enter-email-psw-to-connect: Renseignez votre adresse email et votre mot de passe pour vous connecter.
 };
 
 module.exports = { data };
