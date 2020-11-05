2d6e81cc795fed30a6bc6d58cfedc74f96af319a
Antonio Pierro <antonio.pierro@gmail.com>
2020-11-05 12:46:20 +0100

    adding dotJs

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
+const doT = require("./doT");
+
 const component = () => {
     const element = globalThis.document.createElement("div");
     element.innerHTML = "Hello3";
+    console.log(doT);
     return element;
 };
 
