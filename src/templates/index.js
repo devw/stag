4af71d555250c3cb0257db19c932ae5c95594339
Antonio Pierro <antonio.pierro@gmail.com>
2020-11-10 16:40:04 +0100

    adding login-template controller

--- src/templates/index.js
+++ src/templates/index.js
@@ -2,10 +2,12 @@ const rootTmpl = require("./root.html");
 const containerTmpl = require("./container.html");
 const landingTmpl = require("./landing.html");
 const registerTmpl = require("./register.html");
+const signInTmpl = require("./sign-in.html");
 const {
     ROOT_ID,
     LANDING_ID,
     REGISTER_ID,
+    SIGNIN_ID,
     CONTAINER_ID,
 } = require("../configs/pages-id.yml");
 
@@ -17,5 +19,6 @@ templates.push(getTemplate({ id: ROOT_ID, html: rootTmpl }));
 templates.push(getTemplate({ id: CONTAINER_ID, html: containerTmpl }));
 templates.push(getTemplate({ id: LANDING_ID, html: landingTmpl }));
 templates.push(getTemplate({ id: REGISTER_ID, html: registerTmpl }));
+templates.push(getTemplate({ id: SIGNIN_ID, html: signInTmpl }));
 
 module.exports = { templates };
