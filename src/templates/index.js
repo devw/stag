c36ecb673c6703ee40a1ca20bfb80198f619f8ba
Antonio Pierro <antonio.pierro@gmail.com>
2020-11-10 16:07:20 +0100

    adding register-carousel

--- src/templates/index.js
+++ src/templates/index.js
@@ -2,10 +2,12 @@ const rootTmpl = require("./root.html");
 const containerTmpl = require("./container.html");
 const landingTmpl = require("./landing.html");
 const registerTmpl = require("./register.html");
+const registerCarouselTmpl = require("./register-carousel.html");
 const {
     ROOT_ID,
     LANDING_ID,
     REGISTER_ID,
+    REGISTER_CAROUSEL_ID,
     CONTAINER_ID,
 } = require("../configs/pages-id.yml");
 
@@ -17,5 +19,8 @@ templates.push(getTemplate({ id: ROOT_ID, html: rootTmpl }));
 templates.push(getTemplate({ id: CONTAINER_ID, html: containerTmpl }));
 templates.push(getTemplate({ id: LANDING_ID, html: landingTmpl }));
 templates.push(getTemplate({ id: REGISTER_ID, html: registerTmpl }));
+templates.push(
+    getTemplate({ id: REGISTER_CAROUSEL_ID, html: registerCarouselTmpl })
+);
 
 module.exports = { templates };
