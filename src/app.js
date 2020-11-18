942bdc5e52e8f9b7b83d4373ae2a0a123b054f9c
Antonio Pierro <antonio.pierro@gmail.com>
2020-11-18 14:45:11 +0100

    adding is isPopover condition

--- src/app.js
+++ src/app.js
@@ -9,9 +9,21 @@ const node = document.createElement("div");
 node.setAttribute("id", APP_ID);
 document.body.append(node);
 
+const placePopover = (e) => {
+    const dom = node.querySelector(".container");
+    const shift = parseInt(getComputedStyle(dom).getPropertyValue("width"));
+    dom.style.setProperty("left", `${e.clientX - shift / 2}px`);
+    dom.style.setProperty("top", `${e.clientY + 20}px`);
+};
+
 const $ = document.querySelector.bind(document);
 const openAccount = (e) => {
     e.preventDefault();
-    node.querySelector(".container").style.setProperty("display", "flex");
+    e.stopPropagation();
+    const dom = node.querySelector(".container");
+    dom.style.setProperty("display", "flex");
+    dom.classList.contains("popover")
+        ? placePopover(e)
+        : document.body.classList.add("remove-scrolling");
 };
 $(".site-header__account").addEventListener("click", openAccount);
