b6e1e253e03563ab41da960a407918b948e0f3bc
Antonio Pierro <antonio.pierro@gmail.com>
2020-11-20 11:14:26 +0100

    building

--- src/actions/register.js
+++ src/actions/register.js
@@ -53,6 +53,7 @@ const onSubmit = async (e) => {
     $q(tgt.form).action = "/account/register";
     const resp = await sendHttpRequest("POST", e);
     console.log("shopify response.....", resp);
+    $q(tgt.form).submit();
 
     // globalThis.__form = form;
     // globalThis.__inputs = inputs;
