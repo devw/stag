9721a64cd70c0a8c492c947ea63f7d9470937487
Antonio Pierro <antonio.pierro@gmail.com>
2020-11-27 12:31:01 +0100

    fixing issue with warehouse template

--- src/app.js
+++ src/app.js
@@ -10,8 +10,7 @@ const node = document.createElement("div");
 node.setAttribute("id", APP_ID);
 document.body.append(node);
 
-const placePopover = (e) => {
-    const dom = node.querySelector(".container");
+const placePopover = (e, dom) => {
     const shift = parseInt(getComputedStyle(dom).getPropertyValue("width"));
     dom.style.setProperty("left", `${e.clientX - shift / 2}px`);
     dom.style.setProperty("top", `${e.clientY + 20}px`);
@@ -21,8 +20,10 @@ const $ = document.querySelector.bind(document);
 const openAccount = (e) => {
     e.preventDefault();
     e.stopPropagation();
-    const dom = node.querySelector(".container");
-    dom.style.setProperty("display", "flex");
-    if (dom.classList.contains("popover")) placePopover(e);
+    if (globalThis.__st?.cid) {
+        const dom = node.querySelector(".container");
+        dom.style.setProperty("display", "flex");
+        if (dom.classList.contains("popover")) placePopover(e, dom);
+    }
 };
 $(".site-header__account").addEventListener("click", openAccount);
