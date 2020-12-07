52f5380602cae8bb4d1ab34ae5ddb80a4da288ad
Antonio Pierro <antonio.pierro@gmail.com>
2020-12-07 15:52:17 +0100

    building

--- src/services/kastor.js
+++ src/services/kastor.js
@@ -5,7 +5,7 @@ const kastorHandler = (event) => {
     const { setting_id, value } = body;
     console.log("setting_id, value: ", setting_id, value);
     console.log("Event received from Customize", setting_id, value);
-    if (/font-size/.test(value)) updateCss({ [setting_id]: `${value}em` });
+    if (/font-size/.test(setting_id)) updateCss({ [setting_id]: `${value}em` });
 };
 
 exports.kastor = () => {
