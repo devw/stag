3aedc9c1c64f906c77ab34ba9e6873b535f6b86e
Antonio Pierro <antonio.pierro@gmail.com>
2020-12-09 17:59:28 +0100

    building

--- src/services/kastor.js
+++ src/services/kastor.js
@@ -20,13 +20,6 @@ const sectionSettings = ({ sectionName, setting_id, value }) => {
     $q(".container").style.setProperty("display", "flex");
 };
 
-globalThis.__updatePages = (json) => {
-    updatePages(json);
-    const section_type_id = "landing-section";
-    toggleModules(section_type_id.replace(/-section/, ""));
-    $q(".container").style.setProperty("display", "flex");
-};
-
 const generalSettings = ({ setting_id, value }) => {
     if (/font-size|text-size|border-radius/.test(setting_id)) {
         console.log("Event received from Customize", setting_id, value);
@@ -53,9 +46,7 @@ const kastorHandler = (event) => {
 const changeTheme = (themeName) =>
     getTheme(themeName).then((theme) => updateCss(theme.style));
 
-globalThis.__changeTheme = changeTheme;
-
 exports.kastor = () => {
     console.log("loading kastor handler");
-    globalThis.addEventListener("message", kastorHandler);
+    globalThis.addEventListener("message", (e) => debounce(kastorHandler, 500));
 };
