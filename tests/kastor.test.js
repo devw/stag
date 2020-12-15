a628b51bb1eabe4bfacf76adc8006c858dbcf654
Antonio Pierro <antonio.pierro@gmail.com>
2020-12-15 15:48:47 +0100

    adding testing

---
+++ tests/kastor.test.js
@@ -0,0 +1,44 @@
+(() => {
+    let kastorEvent;
+
+    const textEvent = {
+        setting_id: "landing|loginRegister|",
+        value: "Creazione di login",
+    };
+
+    const layout = {
+        setting_id: "landing|layout|",
+        value: "popup-floating",
+    };
+
+    const style = {
+        setting_id: "|--large-font-size|em",
+        value: "3.3",
+    };
+
+    const hasCarousel = {
+        setting_id: "register|hasCarousel|",
+        value: true,
+    };
+
+    const animation = {
+        setting_id: "landing|--animation|",
+        value: "slide-in",
+    };
+
+    const changeTheme = {
+        setting_id: "|change-theme|",
+        value: "respire",
+    };
+
+    const socialLogin = {
+        setting_id: "landing|hasSocialLogin|",
+        value: true,
+    };
+
+    kastorEvent = new CustomEvent("message", {
+        detail: animation,
+    });
+
+    globalThis.dispatchEvent(kastorEvent);
+})();
