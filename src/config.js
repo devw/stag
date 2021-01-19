38f888f014bd52f268a2a1d2d58591051b668f5d
Antonio Pierro <antonio.pierro@gmail.com>
2021-01-19 11:26:30 +0100

    adding config-endpoint

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 module.exports = {
-    //AWS_ENDPOINT: "http://ec2-3-19-56-186.us-east-2.compute.amazonaws.com",
-    AWS_ENDPOINT: "https://f7b45b79bb90.ngrok.io",
+    CONFIG_ENDPOINT: "https://login-popup-dev-theme.s3.amazonaws.com",
     STAG_ENDPOINT: "./",
     LOGIN_BTN: ".site-header__account",
     PROXY_PATH: "apps/dev",
