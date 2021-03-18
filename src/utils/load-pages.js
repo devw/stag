39348916767ff4b02ea31ab1f362d3f312ca4cd3
Antonio Pierro <antonio.pierro@gmail.com>
2021-03-18 17:42:34 +0100

    fixing typo

--- src/utils/load-pages.js
+++ src/utils/load-pages.js
@@ -12,7 +12,7 @@ const {
 const HEAD = document.getElementsByTagName("head")[0];
 
 const updateInputFields = () => {
-    // you should moce this piece of code in render method otherwise it will be not visible on kastor
+    // you should move this piece of code in render method otherwise it will be not visible on kastor
     const pswElem = $q("[name='customer[password]']");
     let newNode = document.createElement("i");
     newNode.classList.add("fa", "fa-eye", "js-show-psw");
