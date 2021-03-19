3eace1b0e2a2b42e3c12c065a3f5b415ef59daf3
Antonio Pierro <antonio.pierro@gmail.com>
2021-03-19 16:35:16 +0100

    fixing the input.checker.test

--- tests/src/utils/input.checker.test.js
+++ tests/src/utils/input.checker.test.js
@@ -3,24 +3,16 @@ const formHtml = require("./__mocks__/form.mock.html");
 jest.mock("../../../src/utils/toggle", () => require("./__mocks__/index.mock"));
 
 describe("Proxy Test", () => {
-    test.only("It should give an error if the password length is less than 5 characters", async () => {
-        const form = document.createElement("form");
-        form.innerHTML = formHtml;
-        console.log("form.innerHTML.....", form.innerHTML, form);
-        document.body.appendChild(form);
-        const resetErrorMsgs = jest.fn(() => 1);
+    const form = document.createElement("form");
+    form.innerHTML = formHtml;
+    document.body.appendChild(form);
+    it("It should give an error if the password length is less than 5 characters", async () => {
+        form.querySelector("[type='password']").value = "123";
         expect(areInvalidInputs()).toBe(true);
     });
 
-    it("should be false if the password length is less than the pswMinLength", async () => {
-        globalThis.localStorage.setItem(
-            "shopify-loginpopup-configuration",
-            JSON.stringify({
-                text: { pswMinLength: 5 },
-            })
-        );
-        const resetErrorMsgs = jest.fn(() => 1);
-        const result = areInvalidInputs(this.form);
-        expect(result).toBe(false);
+    it("It should give no error if the password length is greater than 5 characters", async () => {
+        form.querySelector("[type='password']").value = "1234567";
+        expect(areInvalidInputs()).toBe(false);
     });
 });
