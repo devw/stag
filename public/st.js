738b4d335a8529f173568be1a700e7ef3674af38
Antonio Pierro <antonio.pierro@gmail.com>
2021-07-26 14:49:34 +0200

    fix: fix style of template 1 and 2

--- public/st.js
+++ public/st.js
@@ -1,10 +1,13 @@
-
-const HEAD = document.getElementsByTagName("head")[0];
-
-// TODO fix with Shopify.shop
 (() => {
+    window.onClick_loginPopup = (e) => e.preventDefault();
+    window.el__loginPopup = document.body.querySelector('[href="/account/login"]');
+    window.el__loginPopup?.addEventListener("click", window.onClick_loginPopup, true);
+
+    const HEAD = document.getElementsByTagName("head")[0];
+
+    // TODO fix with Shopify.shop
     const ENDPOINT = globalThis?.Shopify
-        ? "https://login-popup-dev-theme.s3.amazonaws.com"
+        ? "https://login-popup-dev-configs.s3.amazonaws.com/public"
         : "http://localhost:8082";
 
     const addCSS = (url) => {
@@ -18,7 +21,7 @@ const HEAD = document.getElementsByTagName("head")[0];
         HEAD.appendChild(script);
     };
 
-    addJS(`${ENDPOINT}/bundle.js?t=${Math.random()} `);
+    addJS(`${ENDPOINT}/bundle.js?t=${Math.random()}`);
     addCSS(`${ENDPOINT}/main.css?t=${Math.random()}`);
     addCSS(
         "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
--- src/app.js
+++ src/app.js
@@ -6,9 +6,12 @@ require("./styles/popup.scss");
 require("./styles/register.scss");
 const { loadTheme } = require("./actions");
 const { IDs } = require("./config");
+console.log("----s")
 
-const node = document.createElement("div");
-node.setAttribute("id", IDs.APP_ID);
-document.body.append(node);
+setTimeout(() => {
+    const node = document.createElement("div");
+    node.setAttribute("id", IDs.APP_ID);
+    document.body.prepend(node);
+    loadTheme();
+}, 0);
 
-loadTheme();
