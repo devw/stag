f569d001c85ab936865afc163a24777a5b9d9368
Antonio Pierro <antonio.pierro@gmail.com>
2021-09-06 06:43:55 +0200

    refactor: from doublequotes to singlequotes

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,21 @@
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = (env) => ({
-    entry: env?.in || "./src/app.js",
+    entry: env?.in || './src/app.js',
     output: {
-        filename: env?.out || "bundle.js",
-        path: path.resolve(path.resolve(), "dist"),
+        filename: env?.out || 'bundle.js',
+        path: path.resolve(path.resolve(), 'dist'),
     },
     module: {
         rules: [
             {
                 test: /\.html$/i,
-                loader: "html-loader",
+                loader: 'html-loader',
             },
             {
                 test: /\.s[ac]ss$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
             },
         ],
     },
