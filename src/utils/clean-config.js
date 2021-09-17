8df78bccf59947d57112cde817164ac90adfd105
Antonio Pierro <antonio.pierro@gmail.com>
2021-09-17 22:14:52 +0200

    style: use singleQuotes

--- src/utils/clean-config.js
+++ src/utils/clean-config.js
@@ -1,19 +1,19 @@
 exports.cleanChoiceBlock = (text) => {
     //TODO this part should be made in kastor
     text?.choiceBlocks?.forEach((e) => {
-        e.lineStyle = e.hasMultiLine ? "multi-line" : "";
-        e.choiceStyle = e.hasMultiChoice ? "multi-choice" : "single-choice";
-        e.isTag = e.isTag === "hasTag" ? true : false;
+        e.lineStyle = e.hasMultiLine ? 'multi-line' : '';
+        e.choiceStyle = e.hasMultiChoice ? 'multi-choice' : 'single-choice';
+        e.isTag = e.isTag === 'hasTag' ? true : false;
     });
 
     return text;
 };
 exports.cleanInputBlocks = (text) => {
     text?.inputBlocks?.forEach((e) => {
-        if (e.name === "customer[email]") {
+        if (e.name === 'customer[email]') {
             e.placeholder = text?.yourEmail;
         }
-        if (e.name === "customer[password]") {
+        if (e.name === 'customer[password]') {
             e.placeholder = text?.yourPassword;
         }
     });
@@ -23,7 +23,7 @@ const pastDate = (year) => {
     const mSecYear = 31_536_000_000;
     const seconds = Date.now() - year * mSecYear;
     const d = new Date(seconds);
-    return [d.getDate(), d.getMonth() + 1, d.getFullYear()].join("-");
+    return [d.getDate(), d.getMonth() + 1, d.getFullYear()].join('-');
 };
 
 const cleanDate = (date) => {
@@ -34,7 +34,7 @@ const cleanDate = (date) => {
 exports.cleanDateBlocks = (text) => {
     //TODO this part should be made in kastor
     text?.dateBlocks?.forEach((e) => {
-        e.isTag = e.isTag === "hasTag" ? true : false;
+        e.isTag = e.isTag === 'hasTag' ? true : false;
         e.minDate = cleanDate(e.minDate);
         e.maxDate = cleanDate(e.maxDate);
     });
@@ -43,21 +43,22 @@ exports.cleanDateBlocks = (text) => {
 };
 
 const mendInputPadding = (padding, { formStyle }) => {
-    if (formStyle === "label-go-up") {
-        padding.splice(0, 1, "18px");
-        padding.splice(2, 1, "7px");
-        return padding.join(" ");
+    if (formStyle === 'label-go-up') {
+        padding.splice(0, 1, '18px');
+        padding.splice(2, 1, '7px');
+        return padding.join(' ');
     }
-    if (formStyle === "label-go-down") {
-        padding.splice(0, 1, "8px");
-        padding.splice(2, 1, "12px");
-        return padding.join(" ");
+    if (formStyle === 'label-go-down') {
+        padding.splice(0, 1, '8px');
+        padding.splice(2, 1, '12px');
+        return padding.join(' ');
     }
-    return padding.join(" ");
+    return padding.join(' ');
 };
 
 exports.cleanStyle = (style, text) => {
-    const inputPadding = style["--input-padding"].trim().split(" ");
-    style["--input-padding"] = mendInputPadding(inputPadding, text);
+    console.log('cleanStyle:');
+    const inputPadding = style['--input-padding'].trim().split(' ');
+    style['--input-padding'] = mendInputPadding(inputPadding, text);
     return style;
 };
